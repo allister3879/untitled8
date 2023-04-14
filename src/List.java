28a6e6bc4057b31c0b748130869a32e2c87e7722
allister3879 <220956@astanait.edu.kz>
2023-04-14 15:50:01 +0600

    implementation of remove method

--- src/List.java
+++ src/List.java
@@ -1,5 +1,6 @@
 public interface List <T> {
     void add(T item);
     T getElement(int index);
+    void remove(int index);
 
 }
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,6 +1,10 @@
 public class MyArrayList<T> implements List{
     private T[] arr;
     private int size;
+    MyArrayList(){
+        this.arr = (T[]) new Object[5];
+        this.size = 0;
+    }
     @Override
     public void add(Object item) {    // item - element to add
         if(size == arr.length) // array is full
@@ -12,6 +16,15 @@ public class MyArrayList<T> implements List{
     public T  getElement(int index) {
         checkIndex(index); // method to check index
         return arr[index];  // returns element at specified index
+    }
+
+    @Override
+    public void remove(int index) {
+        checkIndex(index);  // method to check index
+        for(int i= index + 1; i<size; i++){  // create for loop to work with elements starting from special index
+            arr[i-1] = arr[i];   // reassign element to previous index
+        }
+        size--;  // decrease size
     }
 
     public void increaseBuffer(){
